816b5ed6ea4597a4cc16e720e78fa88d17d32270
sinster2003 <sindhurvshabaraya2318@gmail.com>
2024-02-12 00:03:42 +0530

    styling input components

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,28 +1,50 @@
 import { Flex, Input, Select, Text } from "@chakra-ui/react";
-import Subheading from './../components/utils/Subheading';
+import Subheading from "./../components/utils/Subheading";
 import Subtext from "../components/utils/Subtext";
 import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <Flex flexDirection="column" alignItems="center" mt={10}>
-      <Subheading text="Welcome Back" size="4xl"/>
-      <Subtext text="Delve into the adventurous gaming universe in our easy-to-use platform" margin={2} center="center"/>
-      <Text mt={2} mb={8}>New To GameQuest? <Link to="/signup">Sign Up</Link></Text>
-      <Flex flexDirection="column" gap={6} w={{base: 300, md: 400}}>
-        <Input placeholder="Enter Username" variant="filled" focusBorderColor="purple.shadowLight"/>
-        <Input placeholder="Enter Password" variant="filled" focusBorderColor="purple.shadowLight"/>
-        <Select variant="filled" focusBorderColor="purple.shadowLight">
-          <option>
-            Customer
-          </option>
-          <option>
-            Owner
-          </option>
+      <Subheading text="Welcome Back" size="4xl" />
+      <Subtext
+        text="Delve into the adventurous gaming universe in our easy-to-use platform"
+        margin={2}
+        center="center"
+      />
+      <Text mt={2} mb={8}>
+        New To GameQuest? <Link to="/signup">Sign Up</Link>
+      </Text>
+      <Flex flexDirection="column" gap={6} w={{ base: 300, md: 400 }}>
+        <Input
+          placeholder="Enter Username"
+          variant="filled"
+          bg="#2d2a39"
+          _placeholder={{ color: "#5a555e" }}
+          _hover={{}}
+          focusBorderColor="purple.shadowLight"
+        />
+        <Input
+          placeholder="Enter Password"
+          type="password"
+          variant="filled"
+          bg="#2d2a39"
+          _placeholder={{ color: "#5a555e" }}
+          _hover={{}}
+          focusBorderColor="purple.shadowLight"
+        />
+        <Select
+          variant="filled"
+          focusBorderColor="purple.shadowLight"
+          bg="#2d2a39"
+          _hover={{}}
+        >
+          <option style={{ backgroundColor: "#2d2a39" }}>Customer</option>
+          <option style={{ backgroundColor: "#2d2a39" }}>Owner</option>
         </Select>
       </Flex>
     </Flex>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
