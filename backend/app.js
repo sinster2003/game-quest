df24a8c97ab3f1f1bf3a1d66f84d2d4aa11ba20f
sinster2003 <sindhurvshabaraya2318@gmail.com>
2024-03-01 13:11:24 +0530

    build

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import {v2 as cloudinary} from "cloudinary";
 import { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_NAME } from "./utils/config.js";
+import path from "path";
 
 connectDb(); // connecting database
 
@@ -28,11 +29,17 @@ app.use(cors(corsOption)); // avoid cors errors
 app.use(express.json({limit: "50mb"})); // access req.body
 app.use(express.urlencoded({extended: true, limit: "50mb"})); // parses form data
 app.use(cookieParser());
+app.use(express.static()); // to handle static build files 
 
 // version router
 app.use("/api/v1", apiRouter);
 
 // error handler
 app.use(errorHandler);
+
+// deployed route handler
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+})
 
 export default app;
