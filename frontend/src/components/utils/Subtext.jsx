59b4f3868fb378aba566002540262299a6d8ea22
sinster2003 <sindhurvshabaraya2318@gmail.com>
2024-03-01 13:45:21 +0530

    build fix

--- frontend/src/components/utils/Subtext.jsx
+++ frontend/src/components/utils/Subtext.jsx
@@ -4,7 +4,7 @@ const Subtext = ({text, margin, center, size, weight}) => {
   const [isMobileOrTab] = useMediaQuery('(max-width: 900px)');
 
   return (
-    <Text fontSize={size ? size : "md"} my={margin ? margin : 6} textAlign={isMobileOrTab && "center"} w={{base: 300, md: 500}} bgGradient={"linear(to-r, white.light, white.dark)"} bgClip="text" fontWeight={weight && weight}>
+    <Text fontSize={size ? size : "md"} my={margin ? margin : 6} textAlign={isMobileOrTab && "center" || center && center} w={{base: 300, md: 500}} bgGradient={"linear(to-r, white.light, white.dark)"} bgClip="text" fontWeight={weight && weight}>
       {text}
     </Text>
   )
